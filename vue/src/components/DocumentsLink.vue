a8d9646410f0b5f7c20a678f5e14b3ab4af8e0c0
Kaoru Chisen <cordx56@cordx.net>
2019-12-05 01:55:53 +0900

    add documents link

---
+++ vue/src/components/DocumentsLink.vue
@@ -0,0 +1,30 @@
+<template lang="pug">
+  section.docuemtsLink
+    h3 Documents
+    b-table-simple
+      b-tbody
+        b-tr
+          b-th Name
+          b-th Description
+        b-tr(v-for="(v, k, i) in docslist", :key="i")
+          b-td(v-if="v.link")
+            a(:href="v.link", target="_blank") {{ v.name }}
+          b-td(v-else) {{ v.name }}
+          b-td {{ v.desc }}
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      docslist: [
+        {
+          name: 'tf-idfとWord2Vecの併用による文章間類似度判定の実装',
+          link: 'https://speakerdeck.com/cordx56/tf-idftoword2vecfalsebing-yong-niyoruwen-zhang-jian-lei-si-du-pan-ding-falseshi-zhuang',
+          desc: '発表資料 - April 22, 2019'
+        }
+      ]
+    }
+  }
+}
+</script>
--- vue/src/views/Home.vue
+++ vue/src/views/Home.vue
@@ -5,17 +5,20 @@
         img(:src="mainImageSrc")
       b-col(cols="12", lg="8")
         DigicoForm.mb-4(@updateImage="updateMainImage")
+        DocumentsLink
         AboutInfo
 </template>
 
 <script>
 import DigicoForm from '@/components/DigicoForm.vue'
+import DocumentsLink from '@/components/DocumentsLink.vue'
 import AboutInfo from '@/components/AboutInfo.vue'
 
 export default {
   name: 'home',
   components: {
     DigicoForm,
+    DocumentsLink,
     AboutInfo
   },
   data() {
